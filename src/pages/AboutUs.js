4040d086e8ea261d1ad89a36b05ece776464c0ef
Frederik Luca Heimes <heimesfrederik@gmail.com>
2023-09-19 21:43:19 +0200

    4. FAQ Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { AbouSection } from "../components/AboutSection";
 import { ServicesSection } from "../components/ServicesSection";
+import { FaqSection } from "../components/FaqSection";
 
 export const AboutUs = () => {
   return (
     <div>
       <AbouSection />
       <ServicesSection />
+      <FaqSection />
     </div>
   );
 };
