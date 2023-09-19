001149f2fb6c8b6391ec7bd68ee4e90a19ccaa30
Frederik Luca Heimes <heimesfrederik@gmail.com>
2023-09-19 21:21:43 +0200

    2. About Section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { AbouSection } from "../components/AboutSection";
+
+export const AboutUs = () => {
+  return (
+    <div>
+      <AbouSection />
+    </div>
+  );
+};
