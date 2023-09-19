16ba6713ede5f9e12d725cca2c4d59d703e67b74
Frederik Luca Heimes <heimesfrederik@gmail.com>
2023-09-19 21:34:49 +0200

    3. Services Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { AbouSection } from "../components/AboutSection";
+import { ServicesSection } from "../components/ServicesSection";
 
 export const AboutUs = () => {
   return (
     <div>
       <AbouSection />
+      <ServicesSection />
     </div>
   );
 };
